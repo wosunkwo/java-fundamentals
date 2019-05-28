d060ed99d77b79b42252150ac574c6c13c2ccea2
williams osunkwo <wosunkwo@gmail.com>
2019-05-28 10:35:35 -0700

    finished the first 2 problems

---
+++ basics/Main.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+public class Main {
+
+  public static void main (String[] args){
+    int dogCount = 1;
+    int catCount = 2;
+    int mouseCount = 3;
+    // System.out.println("i have " + dogCount + " " + pluralize("dog", dogCount));
+    // System.out.println("i have " + catCount + " " + pluralize("cat", catCount));
+    // System.out.println("i have " + mouseCount + " " + pluralize("mouse", mouseCount));
+ 
+    System.out.println(flipNHeads(5));
+  }
+
+  public static String pluralize(String word, int num){
+    if(num == 0 || num > 1){
+      return word + 's';
+    }else {
+      return word;
+    }
+  }
+
+  public static String flipNHeads(int n){
+    int cnt = 0;
+    int min = 0;
+    int max = 1;
+    int numOfFlips = 0;
+    
+    String pluralWord = pluralize("head", n);
+    while(cnt < n){
+      double randNum = min + (double)(Math.random() * ((max - min)));
+      if(randNum < 0.5){
+        System.out.println("tails");
+        cnt = 0;
+      }else{
+        System.out.println("heads");
+        cnt++;
+      }
+      numOfFlips++;
+    }
+    return "It took " + numOfFlips + " " + pluralize("flip", numOfFlips) + " to flip " + n + " " + pluralize("head", n) + " in a row";
+  }
+}
