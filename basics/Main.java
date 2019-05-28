94ca2096657a101ab91f98b025ad0dfcfc586829
williams osunkwo <wosunkwo@gmail.com>
2019-05-28 11:47:49 -0700

    fininshed lab

--- basics/Main.java
+++ basics/Main.java
@@ -1,15 +1,16 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
 public class Main {
-
   public static void main (String[] args){
     int dogCount = 1;
     int catCount = 2;
     int mouseCount = 3;
-    // System.out.println("i have " + dogCount + " " + pluralize("dog", dogCount));
-    // System.out.println("i have " + catCount + " " + pluralize("cat", catCount));
-    // System.out.println("i have " + mouseCount + " " + pluralize("mouse", mouseCount));
- 
+    System.out.println("i have " + dogCount + " " + pluralize("dog", dogCount));
+    System.out.println("i have " + catCount + " " + pluralize("cat", catCount));
+    System.out.println("i have " + mouseCount + " " + pluralize("mouse", mouseCount));
     System.out.println(flipNHeads(5));
+    clock();
   }
 
   public static String pluralize(String word, int num){
@@ -26,7 +27,6 @@ public class Main {
     int max = 1;
     int numOfFlips = 0;
     
-    String pluralWord = pluralize("head", n);
     while(cnt < n){
       double randNum = min + (double)(Math.random() * ((max - min)));
       if(randNum < 0.5){
@@ -39,5 +39,19 @@ public class Main {
       numOfFlips++;
     }
     return "It took " + numOfFlips + " " + pluralize("flip", numOfFlips) + " to flip " + n + " " + pluralize("head", n) + " in a row";
+  }
+
+  public static void clock(){
+    LocalDateTime now = LocalDateTime.now();
+    int second = now.getSecond();
+   while(true){
+      now = LocalDateTime.now();
+      int newSecond = now.getSecond();
+      if(second != newSecond){
+        String time = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+        System.out.println(time);
+        second = newSecond;
+      }
+     }  
   }
 }
